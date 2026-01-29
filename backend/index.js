bbd462911b5222582f8a1e9e6db129ff21064cf0
devsagarkumarjha <devsagarkumarjha@gmail.com>
2026-01-29 19:52:42 +0530

    feat(backend): init basic express and socket.io server
    - set up express app for basic http routes
    - integrate socket.io for real-time communication
    - add basic chat room functionality (join and broadcast messages)
    - include `dotenv` for easy environment variable management
    - add `nodemon` for a better dev experience
    - create initial project files: `.env`, `.gitignore`, `package.json`, `package-lock.json`, and `print` utility

---
+++ backend/index.js
@@ -0,0 +1,45 @@
+import { createServer } from "http";
+import express from "express";
+import { Server } from "socket.io";
+import { print } from "./utilities/print.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+const port = process.env.PORT || 3000;
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+const ROOM = "group";
+
+io.on("connection", (socket) => {
+  print(`${socket.id} is connected`);
+
+  socket.on("join-room", async (username) => {
+    console.log(`${username} is joining the ${ROOM}`);
+    await socket.join(ROOM);
+
+    // send to all
+    // io.to("group").emit("roomNotice", username)
+
+    // broadcast
+    socket.to(ROOM).emit("room-notice", username);
+  });
+
+  socket.on("chat-message", (msg) => {
+    socket.to(ROOM).emit("chat-message", msg);
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello Socket<h1>");
+});
+
+server.listen(port, () => {
+  print(`Server is listening at http://localhost:${port}`);
+});
